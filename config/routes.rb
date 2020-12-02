c75774f85544ea16cbc08550a2c1f0cdf939e94f
Subhashini <sb031097@gmail.com>
2020-12-02 07:40:52 +0530

    Add about Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'      #Telling rails to expect a home method in the Pages Controller file
+  get 'about', to: 'pages#about'
   resources :notebooks
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
