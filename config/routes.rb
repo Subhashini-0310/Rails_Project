64dcf8c8bc3e699ed45dbdca9aedfdfe33b2124f
Subhashini-0310 <sb031097@gmail.com>
2020-12-02 05:45:05 +0530

    Initial commit and add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#home'      #Telling rails to expect a home method in the Pages Controller file
   resources :notebooks
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
